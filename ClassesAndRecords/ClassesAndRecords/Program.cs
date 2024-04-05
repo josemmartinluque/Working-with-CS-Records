de2e5a6bc24d4108517ec36934faaf7a40698d5e
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-05 10:03:01 +0200

    Module 2 completed

---
+++ ClassesAndRecords/ClassesAndRecords/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassesAndRecords
+{
+    class Program
+    {
+
+        static void PrintCourse(Course course)
+        {
+            Console.WriteLine(course.Name);
+        }
+
+        static void Main(string[] args)
+        {
+            Course pluralsightCourse = new();
+            pluralsightCourse.Name = "Working with C# Records";
+            pluralsightCourse.Author = "Roland Guijt";
+
+            CourseRecord classroomCourse = new(
+                "Working with C# records",
+                "Roland Guijt"
+                );
+        }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+
+    public record CourseRecord(string Name, string Author);
+}
