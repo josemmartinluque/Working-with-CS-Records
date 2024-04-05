704c73479c48271f309f25c03c87095c1e7628a9
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-05 11:04:40 +0200

    Module 4 completed

--- ClassesAndRecords/ClassesAndRecords/Program.cs
+++ ClassesAndRecords/ClassesAndRecords/Program.cs
@@ -12,13 +12,41 @@ namespace ClassesAndRecords
                 Duration: 90
                 );
 
-            var clonedCourse = classroomCourse with { Name = "John" };
-            var castCourse = (TimedCourseRecord) clonedCourse;
-            var duration = castCourse.Duration;
+            TimedCourseRecord videoCourse = new(
+                "Working with C# Records",
+                "John Doe",
+                Duration: 90
+                );
+
+            Console.WriteLine(classroomCourse == videoCourse);
         }
     }
 
-    public record CourseRecord(string Name, string Author);
+    public record CourseRecord(string Name, string Author)
+    {
+
+    }
+
     public record TimedCourseRecord(string Name, string Author, int Duration):
-        CourseRecord(Name, Author);
+        CourseRecord(Name, Author)
+    {
+        public string Venue { get; set; }
+
+        public virtual bool Equals(TimedCourseRecord other)
+        {
+            return Name == other.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+    internal record AnotherTimedCourseRecord(string Name, string Author, int Duration) : TimedCourseRecord(Name, Author, Duration)
+    {
+
+    }
+
+
 }
