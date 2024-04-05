9af46aa1519f2d354c190a0deec5533b03eb9da0
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-05 10:35:42 +0200

    Custom Record Types added

--- ClassesAndRecords/ClassesAndRecords/Program.cs
+++ ClassesAndRecords/ClassesAndRecords/Program.cs
@@ -16,10 +16,9 @@ namespace ClassesAndRecords
             pluralsightCourse.Name = "Working with C# Records";
             pluralsightCourse.Author = "Roland Guijt";
 
-            CourseRecord classroomCourse = new(
-                "Working with C# records",
-                "Roland Guijt"
-                );
+            CourseRecord classroomCourse = new("Working with C# records");
+
+            CourseRecord anotherCourse = classroomCourse with { Name = "C# Getting Started" };
         }
     }
 
@@ -27,7 +26,27 @@ namespace ClassesAndRecords
     {
         public string Name { get; set; }
         public string Author { get; set; }
+
+        public void Deconstruct(out string name, out string author)
+        {
+            name = Name;
+            author = Author;
+        }
     }
 
-    public record CourseRecord(string Name, string Author);
+    public record CourseRecord(string Name)
+    {
+        public CourseRecord(string name, string author): this(name)
+        {
+            Author = author;
+        }
+        public string Author { get; init; }
+        public string Title
+        {
+            get
+            {
+                return $"{Name} - {Author}";
+            }
+        }
+    }
 }
